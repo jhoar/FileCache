443238b569d829a1812f8248c7f439f464bd545d
jhoar <johnhoar@yahoo.co.uk>
2018-05-30 11:47:52 +0200

    Always call addContext() for when constructing StorageArea for subdirs Switch to debug mode for the time being Update comments

--- FileCache.py
+++ FileCache.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 '''
 
-    To do/think about: 
-    - returning/conflicting sessions, 
+    To do/think about:
+    - returning/conflicting sessions,
     - Multiple openings of the same StorageArea
     - locking contexts
+    - test getFile('file://')
+    - VOSpace as a real file system??
     - many more tests
-'''        
+'''
 
 import json
 import os
@@ -20,31 +22,31 @@ import logging
 
 class StorageArea(object):
     '''
-    StorageArea containing one or more contexts. 
+    StorageArea containing one or more contexts.
     '''
-        
+
     def __init__(self, path: str):
         '''
-        Opens a Storage area, creating the area if necessary 
-        '''        
+        Opens a Storage area, creating the area if necessary
+        '''
         self.contexts = {}
         self.storagePath = None
         self.writable = True
-        
+
         # set up logger
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.DEBUG)
         self.logger = logging.getLogger('FileCache')
-        self.logger.setLevel(logging.INFO)
-                
+        self.logger.setLevel(logging.DEBUG)
+
         # Convert string to a Path
         self.logger.debug("S: " + "Opening " + path)
         self.storagePath = pathlib.Path(path)
-        
+
         # If directory does not exist, create it
         if not self.storagePath.exists():
             self.logger.info("S: " + str(self.storagePath) + " does not exist, creating")
-            self.storagePath.mkdir()    
-        
+            self.storagePath.mkdir()
+
         # Check that we are ready to proceed
         if not self.storagePath.is_dir():
             raise IOError('Storage area is not a directory')
@@ -56,37 +58,39 @@ class StorageArea(object):
 
         # find existing Contexts and instantiate them Contexts
         # Get all directories
+        self.logger.debug("S: Checking subdirectories")
         for subDir in self.storagePath.iterdir():
             if subDir.is_dir():
+                self.logger.debug("S: Found directory in " + str(subDir.stem))
+                ctxt = self.addContext(str(subDir.stem), False)
                 desc = subDir / "desc.json"
                 if desc.is_file():
-                    self.logger.debug("S: Found Context in " + str(subDir.stem))
-                    ctxt = self.addContext(str(subDir.stem), False)
+                    self.logger.debug("S: Load context")
                     ctxt.load(str(desc))
 
     def addContext(self, name: str, createDir: bool = True):
         '''
         Add a context
         '''
-        
+
         self.logger.debug("S: Adding context " + name)
-        
+
         if(name in self.contexts):
             self.logger.debug("S: Context " + name + " already loaded")
             return self.contexts[name]
-        
-        # Create a Path object     
+
+        # Create a Path object
         saneName = format_filename(name)
-        
+
         # Create the directory
         dirPath = self.storagePath / saneName
         if createDir is True:
             if not dirPath.exists():
                 dirPath.mkdir();
-                
-        # Add to the dictionary        
+
+        # Add to the dictionary
         self.contexts[name] = Context(name, dirPath, self)
-              
+
         return self.contexts[name]
 
 
@@ -101,11 +105,11 @@ class StorageArea(object):
         self.logger.debug("S: Purge context " + name)
         self.contexts[name].purge()
         self.contexts[name].deleteDescriptor()
-        self.contexts[name].path.rmdir()
-                
-        # Remove the context from the list        
+        self.contexts[name].path.rmdir() # TODO What happens if files are still there?
+
+        # Remove the context from the list
         del self.contexts[name]
-   
+
     def listContexts(self):
         '''
         Output the available Contexts
@@ -113,7 +117,7 @@ class StorageArea(object):
         for name, ctxt in self.contexts.items():
             print(name + " " + str(ctxt.path) + " " + ctxt.descriptor['author'])
             ctxt.listFiles()
-    
+
 class Context(object):
     '''
     Context - a collection of files identified by their file names. The source
@@ -128,6 +132,8 @@ class Context(object):
     def addFile(self, url: str, filename: str):
         """
         Add an item to the Context. It does not load it into storage.
+
+        If the store area is not writable, this method will return without updating anything
         """
         if not self.store.writable:
             self.store.logger.error("C: Storage in not writable, aborting")
@@ -137,12 +143,14 @@ class Context(object):
 
          # Update stored desc
         self.writeDescriptor()
-        
+
     def getFile(self, filename: str, overwrite: bool = False):
         '''
-        Return the filename of the file in the storage, retrieving data from a 
+        Return the filename of the file in the storage, retrieving data from a
         remote location and storing it in the local storage if necessary. If overwrite is true
-        the file if retrieved irresepctively of whether it is in local storage
+        the file if retrieved irresepctively of whether it is in local storage.
+
+        If the store area is not writable, this method will return None
         '''
         self.store.logger.debug("C: Get file " + filename + " from storage")
 
@@ -162,39 +170,41 @@ class Context(object):
             if overwrite is False and entry['loaded'] is True:
                 self.store.logger.debug("C: Found existing file " + path)
                 return path
-        
+
         # Either the file is not loaded, or we insist on retrieving it
-            
+
         # Get URL for filename
         url = entry['url']
 
         # Retrieve the file and update records
-        # Determine filename for output file 
+        # Determine filename for output file
         outfile = str(self.path / filename)
-        
+
         # Retrieve data into file
         self.store.logger.info("C: Creating file " + str(outfile) + " from " + url)
         urllib.request.urlretrieve(url, outfile)
 
         entry['path'] = outfile
         entry['loaded'] = True
-        
+
         # Update stored manifest
         self.writeDescriptor()
-        
-        # Return file 
+
+        # Return file
         return entry['path']
 
     def deleteFile(self, filename: str):
         '''
         Delete an item from the Context
+
+        If the store area is not writable, this method will return without deleting the file
         '''
         self.store.logger.debug("C: Delete " + filename + " from context " + self.descriptor['name'])
-        
+
         if not self.store.writable:
             self.store.logger.error("C: Storage in not writable, aborting")
             return
-        
+
                 # If filename does not exist, abort
         if self.descriptor['files'][filename] is None:
             self.store.logger.error("C: File is not known in context, aborting")
@@ -205,16 +215,18 @@ class Context(object):
             self.store.logger.debug("C: Deleting file " + path)
             if os.path.isfile(path):
                 os.remove(path)
-                
+
         # Update dictionary
         del self.descriptor['files'][filename]
-        
+
         # Write descriptor
         self.writeDescriptor()
 
     def refresh(self):
         '''
         Retrieves retrieves any items in the Cache which are not in the storage
+
+        If the store area is not writable, this method will return without updating anything
         '''
         self.store.logger.debug("C: Refreshing context " + self.descriptor['name'])
 
@@ -223,13 +235,14 @@ class Context(object):
             return
 
         for filename, entry in self.descriptor['files'].items():
+            # getFile() takes care of the descriptor
             self.getFile(filename)
 
-        # get() takes care of the descriptor
-     
     def purge(self):
         '''
         Deletes all files in the Cache from local storage
+
+        If the store area is not writable, this method will return without updating anything
         '''
         self.store.logger.debug("C: Purging context " + self.descriptor['name'])
 
@@ -237,38 +250,38 @@ class Context(object):
             self.store.logger.error("C: Storage in not writable, aborting")
             return
 
-        for filename, entry in self.descriptor['files'].items(): 
+        for filename, entry in self.descriptor['files'].items():
             if 'path' in entry:
                 path = entry['path']
                 self.store.logger.debug("C: Deleting file " + path)
                 if os.path.isfile(path):
                     os.remove(path)
-                
+
         # Update dictionary
         self.descriptor['files'].clear()
-        
+
         # Write descriptor
         self.writeDescriptor()
-         
-    def writeDescriptor(self):     
+
+    def writeDescriptor(self):
         '''
         Dump Context metadata as a file
-        '''        
-        desc = str(self.path / "desc.json") 
+        '''
+        desc = str(self.path / "desc.json")
         self.store.logger.debug("C: Writing descriptor in " + desc)
         with open(desc , 'w') as handle:
             handle.write(json.dumps(self.descriptor, indent=4))
             handle.close()
 
-    def deleteDescriptor(self):     
+    def deleteDescriptor(self):
         '''
         Delete Context metadata file
-        '''        
+        '''
         desc = self.path / "desc.json"
         self.store.logger.debug("C: Deleting descriptor " + str(desc))
         desc.unlink()
 
-    def export(self, path: str):     
+    def export(self, path: str):
         '''
         Dump Context metadata as a file
         '''
@@ -277,7 +290,7 @@ class Context(object):
             entry['loaded'] = False
             if 'path' in entry:
                 del entry['path']
-        
+
         self.store.logger.debug("C: Exporting context in " + path)
         with open(path, 'w') as handle:
             handle.write(json.dumps(desc, indent=4))
@@ -285,15 +298,15 @@ class Context(object):
 
     def load(self, location: str, merge: bool = False):
         '''
-        Populate the Context metadata; determining from a file or URL. 
-        
+        Populate the Context metadata; determining from a file or URL.
+
         The files will not be retrieved
-        
+
         If merge is True, the existing list of files in the context will be merged with
         the new files. If False, the new list will replace the existing list.
-        The existing context name and author will always be preserved         
+        The existing context name and author will always be preserved
         '''
-        
+
         if urllib.parse.urlparse(location).scheme in ('http', 'https',):
             self.store.logger.debug("C: Populating context from URL " + location)
             data = urllib.request.urlopen(location)
@@ -315,9 +328,9 @@ class Context(object):
         else:
             self.descriptor['author'] = newDesc['author']
             self.descriptor['files'] = copy.deepcopy(newDesc['files'])
-            
+
         self.writeDescriptor()
-        
+
     def listFiles(self):
         '''
         List the files currently registered in the context
@@ -327,36 +340,44 @@ class Context(object):
                 print("\t" + name + " " + files['url'] + " " + files['path'])
             else:
                 print("\t" + name + " " + files['url'])
-                    
+
 class SimpleCache(object):
     '''
     Simple interface based around one context located a directory
-    EuclidCache located in the user's home directory. This is focused 
+    EuclidCache located in the user's home directory. This is focused
     on loading resources as simply as possibly, e.g.:
-    
-        S = FileCache.SimpleCache()
-        S.load("http://vospace.esac.esa.int/vospace/sh/4807f490cec42f15d1574442881ccb1f1275bd?dl=1")
-        S.get("foo.pdf")
-        ...
-        S.destroy() # Clean up
-    
-    
+
+    import FileCache
+    from astropy.io import fits
+
+    # Store files in $HOME/EuclidCache/files/ or Windows equivalent
+    S = FileCache.SimpleCache()
+
+    # Load in the descriptor containing the contents of a bundle
+    S.load("http://vospace.esac.esa.int/vospace/sh/5e558f8785e775a165124178f045ba7c52e49552?dl=1")
+
+    # List the content
+    S.files()
+
+    # S.get() retrieves the file if necessary and returns the path of the file locally
+    fits_image_filename = fits.open(S.get("ick906030_prev.fits"))
+
     '''
     def __init__(self):
         self.home = pathlib.Path.home()
         self.store = StorageArea(str(self.home / 'EuclidCache'))
         self.context = self.store.addContext('files', True)
-        
+
     def load(self, url: str):
         self.context.load(url, True)
         self.context.refresh()
-    
+
     def get(self, file: str):
         return self.context.getFile(file)
 
     def files(self):
         return self.context.listFiles()
-    
+
     def destroy(self):
         self.context.purge()
         self.store.deleteContext('files')
@@ -366,4 +387,3 @@ def format_filename(s):
     filename = ''.join(c for c in s if c in valid_chars)
     filename = filename.replace(' ','_')
     return filename
-
